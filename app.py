a26404e5eb4e960e2f252e5c217ebcc1d2543aea
chaitanya <97513999+kzuvg@users.noreply.github.com>
2026-04-09 11:03:40 +0530

    Refactor hello_world to home and format health route

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-  return render_template('index.html')
 
 @app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/health')
 def health():
-  return 'Server is UP and RUNNING'
+    return 'Server is UP and RUNNING'
 
-app. run(host= '0.0.0.0', port=80)
+
+app.run(host='0.0.0.0', port=80)
